c5454f761dfbc2a50270f143822cd96575db9ad6
benjamin_cornet <benjamin_cornet@ens.univ-artois.fr>
2024-10-14 14:05:32 +0200

    Fixes #2 - Fin de création de la méthode qui récupère limage de larticle

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import requests
 from flask import Flask
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/article/<article>')
+def recupereImageArticle(article):  # put application's code here
+    r = requests.get(" http://ws.chez-wam.info/" + article)
+    image = r.json()["images"][0]
+    return f"<img width='250px' height='250px' src='{image}'/>"
 
 if __name__ == '__main__':
     app.run()
